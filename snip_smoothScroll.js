b670591ca1c0fc621efc9c259e1d814ebd910f70
gundamboy <rowland.charles@gmail.com>
2014-06-24 21:59:53 -0400

    Initial commit
    initial commit

---
+++ snip_smoothScroll.js
@@ -0,0 +1,21 @@
+/*
+*
+* Place this code snippet inside your jQuery ready function. It will take an anchor tag with an id
+* and do a smooth scroll to that position in the DOM
+* example tag: <a href="#contact">Contact Us</a>
+*/
+
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
